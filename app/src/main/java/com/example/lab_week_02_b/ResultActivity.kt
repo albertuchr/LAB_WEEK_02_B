96998f4722066cf4e84ee7b7053d928e65f0aee5
albertuchr <albertuschristianto27@gmail.com>
2025-09-15 02:50:53 +0700

    Commit No. 5 – Fix ResultActivity to allow resubmitting color code without app restart

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,37 +1,46 @@
 package com.example.lab_week_02_b
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import android.content.Intent
 
 class ResultActivity : AppCompatActivity() {
     companion object {
-        const val COLOR_KEY = "COLOR_KEY"
-        const val ERROR_KEY = "ERROR_KEY"
+        private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val colorCode = intent.getStringExtra(COLOR_KEY)
-        val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
-        val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
+        if (intent != null) {
+            val colorCode = intent.getStringExtra(COLOR_KEY)
 
-        if (colorCode != null) {
+            val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
             try {
                 backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
-                resultMessage.text = getString(R.string.color_code_result_message, colorCode.uppercase())
             } catch (ex: IllegalArgumentException) {
-                val errorIntent = Intent()
-                errorIntent.putExtra(ERROR_KEY, true)
-                setResult(Activity.RESULT_OK, errorIntent)
-                finish()
+                Intent().let { errorIntent ->
+                    errorIntent.putExtra(ERROR_KEY, true)
+                    setResult(Activity.RESULT_OK, errorIntent)
+                    finish()
+                }
             }
+
+            val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
+            resultMessage.text =
+                getString(R.string.color_code_result_message, colorCode?.uppercase())
+        }
+
+        val backButton = findViewById<Button>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
